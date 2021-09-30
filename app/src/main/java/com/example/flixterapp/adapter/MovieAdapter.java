4bf00cc3817fa2c634c744f1b4b08ba75f351190
Aashish Adhikari <a.adhikari6960@gmail.com>
2021-09-29 22:48:48 -0400

    merging 1 to 2

--- app/src/main/java/com/example/flixterapp/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/flixterapp/adapter/MovieAdapter.java
@@ -1,18 +1,24 @@
 package com.example.flixterapp.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixterapp.DetailActivity;
 import com.example.flixterapp.R;
 import com.example.flixterapp.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -47,18 +53,28 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
-
+        RelativeLayout container;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle=itemView.findViewById(R.id.tvTitle);
             tvOverview=itemView.findViewById(R.id.tvOverview);
             ivPoster=itemView.findViewById(R.id.ivPoster);
+            container=itemView.findViewById(R.id.container);
         }
 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+
+                }
+            });
         }
     }
 }
